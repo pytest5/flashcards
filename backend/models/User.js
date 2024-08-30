bd812b3778d9a3f7157cbd905417a6220bb2c9fa
Josh Lim <justdoit.josh@gmail.com>
2024-08-30 11:51:36 +0800

    feat: added user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,16 +1,36 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-    username: { type:String, required: true },
-    email: { type:String, unique: true, required: true },
-    hashedPassword: { type:String, required: true },
-    isChild: Boolean
-})
+  userName: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "an email address is required"],
+    lowercase: true,
+    trim: true,
+  },
+  hashedPassword: {
+    type: String,
+    required: [true, "a password is required"],
+    match: /^[A-Za-z\d]{3,}$/,
+  }, //bcrypt is alphanumeric only, setting minlength of 3 for test purposes
+  dateOfBirth: { type: Date, required: [true, "date of birth is required"] },
+  role: {
+    type: String,
+    required: true,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+});
 
 userSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-      delete returnedObject.hashedPassword;
-    },
-  });
-  
-  module.exports = model("User", userSchema);
+  transform: (document, returnedObject) => {
+    delete returnedObject.hashedPassword;
+  },
+});
+
+module.exports = model("User", userSchema);
