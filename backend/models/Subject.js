1bc7ce91c5e2d0d7411691f174dea2c2c4133f72
pytest5 <pytest514@gmail.com>
2024-08-31 11:31:03 +0800

    fix: correct subject schema not defined properly

--- backend/models/Subject.js
+++ backend/models/Subject.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
 const subjectSchema = new mongoose.Schema({
-  subjectName: string,
-  lowercase: true, // ensures consistency for us
-  required: true,
+  subjectName: {
+    type: String,
+    lowercase: true, // ensures consistency for us
+    required: true,
+  },
 });
 const Subject = mongoose.model("Subject", subjectSchema);
 
