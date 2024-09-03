ab591c26b8b12e9edd61a01e45d34a8c621bc7dc
pytest5 <pytest514@gmail.com>
2024-09-04 01:46:46 +0800

    feat: add homecontents

--- frontend/src/components/HomeContent/HomeContent.jsx
+++ frontend/src/components/HomeContent/HomeContent.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import styles from "./HomeContent.module.css";
+import { Link } from "react-router-dom";
+
 export default function HomeContent() {
-  return <div>HomeContent</div>;
+  return (
+    <div>
+      <h2>HomeContent</h2>
+      <Link to="dashboard">View more</Link>
+    </div>
+  );
 }
