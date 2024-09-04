11cac4def911853ed115cf902ef442d6e81d1d5d
pytest5 <pytest514@gmail.com>
2024-09-04 10:28:29 +0800

    chore: comment out unused mock data

--- frontend/src/components/StagingArea/StagingArea.jsx
+++ frontend/src/components/StagingArea/StagingArea.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import { ListBox, ListBoxItem } from "react-aria-components";
 import styles from "./StagingArea.module.css";
-import { FaRegCirclePlay } from "react-icons/fa6";
 import PlayButtonIcon from "../PlayButtonIcon/PlayButtonIcon";
 import { HiOutlineSpeakerWave } from "react-icons/hi2";
 import { FaRegStar } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 import { getCardsByDeckId } from "../../services/cardService";
 
-const mockDecks = [
-  { id: 1, answer: "花", audioSrc: "www.test.com" },
-  { id: 2, answer: "草", audioSrc: "www.test.com" },
-  { id: 3, answer: "树", audioSrc: "www.test.com" },
-];
+// const mockDecks = [
+//   { id: 1, answer: "花", audioSrc: "www.test.com" },
+//   { id: 2, answer: "草", audioSrc: "www.test.com" },
+//   { id: 3, answer: "树", audioSrc: "www.test.com" },
+// ];
 
-const mockFrontBackDecks = [
-  { id: 1, front: "React", back: "react is a framework" },
-  { id: 2, front: "Javascript", back: "javascript is a lauguage" },
-  { id: 3, front: "array.filter", back: "array.filter is non-mutating" },
-];
+// const mockFrontBackDecks = [
+//   { id: 1, front: "React", back: "react is a framework" },
+//   { id: 2, front: "Javascript", back: "javascript is a lauguage" },
+//   { id: 3, front: "array.filter", back: "array.filter is non-mutating" },
+// ];
 
 export default function StagingArea() {
   const { deckId } = useParams();
@@ -60,10 +59,7 @@ export default function StagingArea() {
         <span>3 terms</span>
         <div>deck description</div>
       </section>
-      <ListBox
-        items={mockDecks}
-        className={`${styles.container} ${styles.column}`}
-      >
+      <ListBox items={cards} className={`${styles.container} ${styles.column}`}>
         {(item) => (
           <ListBoxItem id={item._id} className={styles.columnBox}>
             <div className={styles.smallCard}>
