37bccf3b33401aba9660f15e3addc3957416f9ac
pytest5 <pytest514@gmail.com>
2024-09-04 13:16:18 +0800

    feat: wire up session page to pull db data

--- frontend/src/components/StudyLayout/StudyLayout.jsx
+++ frontend/src/components/StudyLayout/StudyLayout.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import StudyNavBar from "../StudyNavBar/StudyNavBar";
 import styles from "./StudyLayout.module.css";
+import { getCardsByDeckId } from "../../services/cardService";
 
 export default function StudyLayout() {
   const [step, setStep] = React.useState(0);
   const [score, setScore] = React.useState({ correct: 0, wrong: 0 });
+  const [cards, setCards] = React.useState([]);
 
-  const realData = [
-    { answer: "花", distractors: ["草", "树"] },
-    { answer: "草", distractors: ["花", "树"] },
-    { answer: "树", distractors: ["花", "草"] },
-  ];
+  const { deckId } = useParams();
 
-  const sessionData = realData.map((i) => ({
+  React.useEffect(() => {
+    async function loadDecks() {
+      const fetchedCards = await getCardsByDeckId(deckId);
+      setCards(fetchedCards);
+    }
+    loadDecks();
+  }, [deckId]);
+
+  if (!cards || cards.length === 0) {
+    return <h1>Loading...</h1>;
+  }
+
+  const sessionData = cards.map((i) => ({
     ...i,
     options: [...i.distractors, i.answer],
     isCorrect: null,
