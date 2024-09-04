b9be11d940d9610de36cbd68e1618e77cbd9abbd
pytest5 <pytest514@gmail.com>
2024-09-05 01:09:47 +0800

    fix: fix and restyle profile page

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -3,6 +3,7 @@ import { Button } from "react-aria-components";
 import styles from "./ProfilePage.module.css";
 import NavBar from "../../components/NavBar/NavBar";
 import { getCurrentUser } from "../../services/userService";
+import CustomButton from "../../components/CustomButton/CustomButton";
 
 export default function ProfilePage() {
   const [user, setUser] = React.useState();
@@ -20,13 +21,17 @@ export default function ProfilePage() {
   }
 
   return (
-    <div className={styles.profileFlex}>
+    <div className={styles.profilePageWrapper}>
+      {/* <h1 className={styles.profileGreetings}>Halo {user.userName}!</h1> */}
       <div className={styles.profileImage}>{user.userName[0]}</div>
-      <h2>{user.userName}</h2>
-      <h3>{user.email}</h3>
-      <Button className={styles.myButton}>Logout</Button>
-      <Button>Delete Account</Button>
-      <NavBar className={styles.navPosition} />
+      <div className={styles.profilePageUserInfo}>
+        <div>{user.email}</div>
+      </div>
+
+      <div className={styles.profilePageActions}>
+        <CustomButton>Logout</CustomButton>
+        <CustomButton variant="secondary">Delete</CustomButton>
+      </div>
     </div>
   );
 }
