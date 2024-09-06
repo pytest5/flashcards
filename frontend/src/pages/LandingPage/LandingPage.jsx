d1e56dcef41e89cfa479d2791d6fcc29711c1b14
pytest5 <pytest514@gmail.com>
2024-09-06 17:54:34 +0800

    style: style landing page;

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 import { Button } from "react-aria-components";
 import styles from "./LandingPage.module.css";
 import CustomButton from "../../components/CustomButton/CustomButton";
+import LandingPageIcon from "../../components/LandingPageIcon/LandingPageIcon";
 
 export default function LandingPage() {
   return (
     <div className={styles.landingPage}>
+      <LandingPageIcon />
       <h1 className={styles.welcomeHeader}>Welcome to FlashLearn!</h1>
       <div className={styles.welcomeMsg}>
-        Are you ready to make learning easy and enjoyable? Whether you’re
-        studying for an exam, expanding your vocabulary, or picking up a new
-        hobby, our app provides the tools you need to succeed!
+        Whether you’re studying for an exam, expanding your vocabulary, or
+        picking up a new hobby, our app provides the tools you need to succeed!
       </div>
       <div className={styles.landingActions}>
         <Link to="/signup">
